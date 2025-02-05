bb0c5156f21e852e673c852164457ddf2f72c680
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-02-05 21:28:00 +0500

    Change language

--- Conf/urls.py
+++ Conf/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     path('',include('app_pages.urls',namespace='pages')),
     path('admin/', admin.site.urls),
-)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
